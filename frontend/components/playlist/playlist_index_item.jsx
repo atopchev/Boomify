3b1818ea811cdfa97b5555e593c283c2cbae735c
Angela Topchev <atopchev@appacademy.io>
2019-11-25 09:13:04 -0800

    add artist imgs

--- frontend/components/playlist/playlist_index_item.jsx
+++ frontend/components/playlist/playlist_index_item.jsx
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom';
 
 const PlaylistIndexItem = ({ playlist, imgNum }) => {
 
-    debugger;
     let authorSubtext;
     if (imgNum >= 9) {
-        authorSubtext = <Link className="p-author"> {playlist.username} </Link>;
+        authorSubtext = <Link to="" className="p-author"> {playlist.username} </Link>;
     } 
 
     let imageUrl = `mix${imgNum}`;
@@ -15,12 +14,12 @@ const PlaylistIndexItem = ({ playlist, imgNum }) => {
     return(
         <div className="playlist-index-item-root">
 
-                <Link className="playlist-tile"> 
+            <Link to="" className="playlist-tile"> 
                     <div className="transition-img"></div>
                     <img className="p-img" src={window.images[imageUrl]} />
-                    <Link className="p-title"> {playlist.name} </Link>
+                {/* <Link to="" className="p-title"> {playlist.name} </Link> */}
                 {authorSubtext}
-                </Link>
+            </Link>
         </div>
     );
 };
